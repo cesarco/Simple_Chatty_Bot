942064c38a3cb46d1005f9b4d4d963ecaac35445
cesarco <devcesarco@outlook.es>
2022-07-03 20:45:02 -0500

    Stage 2/5: Print your name

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,7 +1,8 @@
 print('Hello! My name is Aid.')
 print('I was created in 2020.')
-print('Please, remind me you    r name.')
+print('Please, remind me your name.')
+your_name = input()
 
 # reading a name
 
-print('What a great name you have, {your_name}!')
+print(f'What a great name you have, {your_name}!')
