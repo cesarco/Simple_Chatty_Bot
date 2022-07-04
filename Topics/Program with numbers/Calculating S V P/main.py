7206f3f25c79013f6c85fee8086b5997114ea4ce
cesarco <devcesarco@outlook.es>
2022-07-04 15:12:23 -0500

    Calculating S V P

---
+++ Topics/Program with numbers/Calculating S V P/main.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+# put your python code here
+s = 4 * (a + b + c)
+
+S = 2 * ((a * b) + (b * c) + (a * c))
+
+V = a * b * c
+
+print(s)
+print(S)
+print(V)
