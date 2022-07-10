138e3d1186a3c7b8d51c7b1311da83b90864defe
cesarco <devcesarco@outlook.es>
2022-07-09 21:34:07 -0500

    Fahrenheit

--- Topics/Declaring a function/Fahrenheit/main.py
+++ Topics/Declaring a function/Fahrenheit/main.py
@@ -1,3 +1,4 @@
 def fahrenheit_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5/9
-    return round(celsius,3)
+    num = 32
+    celsius = ((fahrenheit - num) * 5 / 9)
+    return round(celsius, 3)
