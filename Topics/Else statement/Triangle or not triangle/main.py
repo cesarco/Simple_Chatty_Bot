2c7b6806e0fb060a7be0d4493fe5049ae6598106
cesarco <devcesarco@outlook.es>
2022-07-10 15:13:27 -0500

    Triangle or not triangle

---
+++ Topics/Else statement/Triangle or not triangle/main.py
@@ -0,0 +1,10 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+suma = num1 + num2 + num3
+
+if suma == 180:
+    print('The triangle is valid!')
+else:
+    print('The triangle is not valid!')
